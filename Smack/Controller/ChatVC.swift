efef874724fd4533584c71c678bfee16c614c1a2
kyleaquino1 <aquink4@unlv.nevada.edu>
2017-07-26 22:50:09 -0700

    Gesture recognizers

--- Smack/Controller/ChatVC.swift
+++ Smack/Controller/ChatVC.swift
@@ -16,8 +16,11 @@ class ChatVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-    menuButton.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
-     
+        menuButton.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+ 
     }
 
 }

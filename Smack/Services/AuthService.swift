5a34941857eb54fc1939a6b9fba41f3b28d811e2
kyleaquino1 <aquink4@unlv.nevada.edu>
2017-07-27 19:16:45 -0700

    Outlets for CreateAccountVC and registering users

--- Smack/Services/AuthService.swift
+++ Smack/Services/AuthService.swift
@@ -76,9 +76,7 @@ class AuthService {
             
         }
         
-        
     }
-    
     
 }
 
---
+++ Smack/View/RoundedButton.swift
@@ -0,0 +1,33 @@
+//
+//  RoundedButton.swift
+//  Smack
+//
+//  Created by Kyle on 7/27/17.
+//  Copyright © 2017 Kyle Aquino. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 3.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+
+    override func awakeFromNib() {
+        self.setupView()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        self.setupView()
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = cornerRadius
+    }
+    
+}

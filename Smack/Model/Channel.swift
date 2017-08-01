444598ecfd7fc230b4979eb0b1607dd75988c501
kyleaquino1 <aquink4@unlv.nevada.edu>
2017-08-01 14:51:13 -0700

    Channel model setup with get request from API

---
+++ Smack/Model/Channel.swift
@@ -0,0 +1,18 @@
+//
+//  Channel.swift
+//  Smack
+//
+//  Created by Kyle on 8/1/17.
+//  Copyright © 2017 Kyle Aquino. All rights reserved.
+//
+
+import Foundation
+
+struct Channel: Decodable {
+    
+    public private(set) var _id: String!
+    public private(set) var name: String!
+    public private(set) var description: String!
+    public private(set) var __v: Int?
+ 
+}
---
+++ Smack/Services/MessageService.swift
@@ -0,0 +1,62 @@
+//
+//  MessageService.swift
+//  Smack
+//
+//  Created by Kyle on 8/1/17.
+//  Copyright © 2017 Kyle Aquino. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+class MessageService {
+    
+    static let instance = MessageService()
+    
+    var channels = [Channel]()
+    
+    func findAllChannels(completion: @escaping CompletionHandler){
+        
+        Alamofire.request(URL_GET_CHANNELS, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: BEARER_HEADER).responseJSON { (response) in
+            
+            if response.result.error == nil {
+                
+                guard let data = response.data else { return }
+                
+                // USING SWIFT 4 DECODABLE
+                
+                do {
+                    self.channels = try JSONDecoder().decode([Channel].self, from: data)
+                } catch let error {
+                    debugPrint(error as Any)
+                }
+                print(self.channels)
+                
+//      USING SWIFTY JSON
+//                if let json = JSON(data: data).array {
+//                    for item in json {
+//
+//                        let name = item["name"].stringValue
+//                        let channelDescription = item["description"].stringValue
+//                        let id = item["_id"].stringValue
+//
+//                        let channel = Channel(channelTitle: name, channelDescription: channelDescription, id: id)
+//                        self.channels.append(channel)
+//                    }
+//
+//                    completion(true)
+//                }
+                completion(true)
+            } else {
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+            
+        }
+        
+    }
+    
+    
+    
+}
